fe104c0bf1e9337711caf882fb74175e839d1614
SURYA BHARATH R <110535432+Surya036@users.noreply.github.com>
2023-06-02 21:22:37 +0530

    Add files via upload

---
+++ Day6task01.java
@@ -0,0 +1,35 @@
+package com.example.Demo1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+//Day-4:
+//Tasks-3.1,3.2,3.3,3.4
+
+public class Day6task01 
+{
+    public static void main( String[] args ) throws InterruptedException
+    {
+
+    	
+    	WebDriverManager.edgedriver().setup();
+		WebDriver driver= new EdgeDriver();
+		String url="https://demo.opencart.com/index.php?route=account/register&language=en-gb";
+		//Launchig the url
+		driver.navigate().to(url);
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//*[@id=\"input-firstname\"]")).sendKeys("ajith");
+		driver.findElement(By.xpath("//*[@id=\"input-lastname\"]")).sendKeys("b");
+		driver.findElement(By.xpath("//*[@id=\"input-email\"]")).sendKeys("aji@12");
+		driver.findElement(By.xpath("//*[@id=\"input-password\"]")).sendKeys("5676");
+	    driver.findElement(By.xpath("//*[@id=\"input-newsletter-yes\"]")).click();
+	    driver.findElement(By.xpath("//*[@id=\"form-register\"]/div/div/button")).click();
+	    Thread.sleep(3000);
+    }
+}
